c0f45e4578cdb0fed1a607bc2c488e3f4fb9f97a
xuquanru <1163990163@qq.com>
2023-01-13 16:22:57 +0800

    Initial commit

---
+++ video-test/src/main/java/io/github/fairyspace/TestApplication.java
@@ -0,0 +1,19 @@
+package io.github.fairyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌道阻且长，行则将至🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ * 🍁 Program: video
+ * 🍁 Description:
+ * 🍁 @author: xuquanru
+ * 🍁 Create: 2023/1/13
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌行而不辍，未来可期🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ **/
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
---
+++ video-test/src/main/java/io/github/fairyspace/controller/HelloController.java
@@ -0,0 +1,21 @@
+package io.github.fairyspace.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌道阻且长，行则将至🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ * 🍁 Program: video
+ * 🍁 Description:
+ * 🍁 @author: xuquanru
+ * 🍁 Create: 2023/1/13
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌行而不辍，未来可期🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ **/
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Mono<String> hello() {
+        return Mono.just("Welcome to reactive world ~");
+    }
+}
